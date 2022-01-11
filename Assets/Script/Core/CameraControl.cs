3a62e06d98b32835183d7dbb79bd374cc58e224b
shreejan pandey <neplay7@gmail.com>
2022-01-11 10:19:05 +0545

    changes for screen resolution and optimization

--- Assets/Script/Core/CameraControl.cs
+++ Assets/Script/Core/CameraControl.cs
@@ -13,14 +13,16 @@ namespace TestTask.Core
         public float aspect = 1.33333f;
         void Start()
         {
+           
             aspect = (float)Screen.width / (float)Screen.height;
+            Camera.main.projectionMatrix = Matrix4x4.Ortho(
+                 -orthographicSize * aspect, orthographicSize * aspect,
+                 -orthographicSize, orthographicSize,
+                  Camera.main.nearClipPlane, Camera.main.farClipPlane);
         }
         private void LateUpdate()
         {
-            Camera.main.projectionMatrix = Matrix4x4.Ortho(
-                   -orthographicSize * aspect, orthographicSize * aspect,
-                   -orthographicSize, orthographicSize,
-                    Camera.main.nearClipPlane, Camera.main.farClipPlane);
+          
             transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z + offsetZ);
         }
     }
